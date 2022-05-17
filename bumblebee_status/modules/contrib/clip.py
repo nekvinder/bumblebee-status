17ab79b1207de9465f8761e14ff94ed4ff3eeaf6
nekvinder <nekvinder.singh@wolterskluwer.com>
2022-05-18 02:39:26 +0530

    open copyq

--- bumblebee_status/modules/contrib/clip.py
+++ bumblebee_status/modules/contrib/clip.py
@@ -5,12 +5,15 @@
 
 import core.module
 import core.widget
+import core.input
 import clipboard
 
 
 class Module(core.module.Module):
     def __init__(self, config, theme):
         super().__init__(config, theme, core.widget.Widget(self.full_text))
+        core.input.register(
+            self, button=core.input.LEFT_MOUSE, cmd='copyq menu')
 
     def full_text(self, widgets):
-        return "Clip:" + ";".join(clipboard.paste().split('\n'))
+        return ";".join(clipboard.paste().split('\n'))
